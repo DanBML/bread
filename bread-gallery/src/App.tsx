f5bee22e511d97135e8192368306392c89050166
Dan Lord <danlord1997@gmail.com>
2025-05-02 19:58:49 +0200

    Small aesthetic updates

--- bread-gallery/src/App.tsx
+++ bread-gallery/src/App.tsx
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import "./App.css";
 import BreadGallery from "./BreadGallery";
 import avatar from "./assets/avatar.webp";
@@ -18,6 +18,10 @@ const GlobalStyle = createGlobalStyle`
     color: #8a4b2a;
     font-size: 1.5rem;
     margin-bottom: 1rem;
+
+    @media (max-width: 500px) {
+      font-size: 1.2rem;
+      }
   }
 
   p {
@@ -33,35 +37,7 @@ function App() {
       <div>
         <h1>🥖 Bread Gallery 🍞</h1>
 
-        <section
-          style={{
-            display: "flex",
-            alignItems: "center",
-            marginBottom: "2rem",
-            gap: "1rem",
-            maxWidth: "800px",
-            margin: "0 auto",
-            padding: "2rem",
-          }}
-        >
-          <img
-            src={avatar}
-            alt="Pixel baker avatar"
-            style={{
-              width: "100px",
-              height: "100px",
-              imageRendering: "pixelated",
-              borderRadius: "8px",
-            }}
-          />
-          <div>
-            <p>
-              Hi, I'm Dan. Sometimes I bake bread... and sometimes I take
-              pictures of my bread... and sometimes, I make a website to show
-              off my pictures. This is one of those times.
-            </p>
-          </div>
-        </section>
+        <AboutSection />
 
         <BreadGallery />
       </div>
@@ -70,3 +46,56 @@ function App() {
 }
 
 export default App;
+
+const AboutSection = () => {
+  return (
+    <Wrapper>
+      <Avatar src={avatar} alt="Cartoon baker avatar" />
+      <Text>
+        <h2>About Me</h2>
+        <p>
+          Hi, I'm Dan. Sometimes I bake bread... and sometimes I take pictures
+          of my bread... and sometimes, I make a website to show off my
+          pictures. This is one of those times.
+        </p>
+      </Text>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  padding: 2rem;
+  max-width: 800px;
+  margin: 0 auto;
+
+  @media (max-width: 640px) {
+    flex-direction: column;
+    text-align: center;
+  }
+`;
+
+const Avatar = styled.img`
+  width: 150px;
+  height: auto;
+  border-radius: 8px;
+`;
+
+const Text = styled.div`
+  max-width: 600px;
+
+  h2 {
+    margin-bottom: 0.5rem;
+    font-family: "Press Start 2P", monospace;
+    font-size: 1.25rem;
+  }
+
+  p {
+    font-family: "VT323", monospace;
+    font-size: 1.2rem;
+    line-height: 1.5;
+    text-align: justify;
+  }
+`;
