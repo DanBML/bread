4fc23de55526fb0d399405003071c936e678c06f
Dan Lord <danlord1997@gmail.com>
2025-05-26 16:29:07 +0100

    Remove CodingText section

--- bread-gallery/src/Pages/CodingPage/CodingPage.tsx
+++ bread-gallery/src/Pages/CodingPage/CodingPage.tsx
@@ -1,10 +1,8 @@
 import { CodingAboutSection } from "./CodingAboutSection";
-import { CodingText } from "./CodingText";
 
 export const CodingPage = () => (
   <div>
     <h1>💻 {"Snack -> Stack"} 💻</h1>
     <CodingAboutSection />
-    <CodingText />
   </div>
 );
